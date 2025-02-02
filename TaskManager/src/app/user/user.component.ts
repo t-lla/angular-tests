38dc0dffaa52d0fcd63f5820dcff883b899fe98d
t-lla <tomas.llano.miro@gmail.com>
2025-02-02 20:29:21 +0100

    @Output and output() testing
    to recognize custom emitted events

--- TaskManager/src/app/user/user.component.ts
+++ TaskManager/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input, computed } from '@angular/core';
+import { Component, EventEmitter, Input, Output, output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,19 +7,19 @@ import { Component, Input, input, computed } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  //@Input({required: true}) avatar!: string;
-  //@Input({required: true}) name!: string;
-  avatar = input.required<string>(); //read-only, not able to be .set
-  name = input.required<string>();
+  @Input({required: true}) id!: string;
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+  
+  // @Output() select = new EventEmitter();
+  select = output<string>();
 
-  imagePath = computed(() => {
-    return 'assets/users/' + this.avatar()
-  });
 
-  // get imagePath() {
-  //  return 'assets/users/' + this.avatar();
-  // }
+  get imagePath() {
+   return 'assets/users/' + this.avatar;
+  }
 
   onSelectUser () {
+    this.select.emit(this.id);
   }
 }
