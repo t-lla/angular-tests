a294193c102b78a33bd90684839f935a87dfcb8c
t-lla <tomas.llano.miro@gmail.com>
2025-02-02 20:10:31 +0100

    temporary signal input
    yet this project will probably go on without them, this is just a reminder as commit

--- TaskManager/src/app/user/user.component.ts
+++ TaskManager/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, input, computed } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,12 +7,19 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input() avatar!: string;
-  @Input() name!: string;
+  //@Input({required: true}) avatar!: string;
+  //@Input({required: true}) name!: string;
+  avatar = input.required<string>(); //read-only, not able to be .set
+  name = input.required<string>();
 
-  get imagePath() {
-    return 'assets/users/' + this.avatar;
+  imagePath = computed(() => {
+    return 'assets/users/' + this.avatar()
+  });
+
+  // get imagePath() {
+  //  return 'assets/users/' + this.avatar();
+  // }
+
+  onSelectUser () {
   }
-
-  onSelectUser () {}
 }
