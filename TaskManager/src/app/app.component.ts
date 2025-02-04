157b4972818b89b6cd049766a5567260f08b6bc0
t-lla <tomas.llano.miro@gmail.com>
2025-02-04 23:18:17 +0100

    task marked complete

--- TaskManager/src/app/app.component.ts
+++ TaskManager/src/app/app.component.ts
@@ -8,13 +8,13 @@ import { TasksComponent } from './tasks/tasks.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent, ],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId?: string;;
+  selectedUserId?: string;
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId)!;
--- TaskManager/src/app/tasks/tasks.component.ts
+++ TaskManager/src/app/tasks/tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 
 @Component({
@@ -13,6 +13,8 @@ export class TasksComponent {
 
   @Input ({required: true}) userId!: string;
   @Input({required: true}) name!: string;
+
+
   tasks = [
     {
       id: 't1',
@@ -41,6 +43,10 @@ export class TasksComponent {
 
   get selectedUserTasks() {
     return this.tasks.filter((task) => task.userId === this.userId);
+  }
+
+  onCompleteTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 
 }
