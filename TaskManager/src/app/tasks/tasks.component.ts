b740d115ffe4a1f6528901ebe66ea4bcb0f2a4bd
t-lla <tomas.llano.miro@gmail.com>
2025-02-05 01:09:14 +0100

    task creation donzo

--- TaskManager/src/app/tasks/tasks.component.ts
+++ TaskManager/src/app/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { NewTaskComponent } from "./new-task/new-task.component";
+import { type NewTaskData } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
@@ -56,6 +57,17 @@ export class TasksComponent {
   }
 
   onCancelAddTask() {
+    this.isAddingTask = false;
+  }
+
+  onAddTask(taskData: NewTaskData) {
+    this.tasks.push({
+      id: new Date().getTime().toString(),
+      userId: this.userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date
+    });
     this.isAddingTask = false;
   }
 }
