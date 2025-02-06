7d971de15c10ad3a35dc3a721d385c0a14c15d3a
t-lla <tomas.llano.miro@gmail.com>
2025-02-06 17:25:12 +0100

    overkill nested modules

---
+++ TaskManager/src/app/tasks/tasks.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { TasksComponent } from './tasks.component';
+import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { SharedModule } from '../shared/shared.module';
+
+
+@NgModule({
+  declarations: [TasksComponent, TaskComponent, NewTaskComponent],
+  imports: [CommonModule, FormsModule, SharedModule], //CommonModule is for pipes
+  exports: [TasksComponent],
+})
+export class TasksModule {}
