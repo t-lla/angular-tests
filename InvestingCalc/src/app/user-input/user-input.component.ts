3730e9bcbec1d0af1659cbab13a8bc1ddc205a0e
t-lla <tomas.llano.miro@gmail.com>
2025-02-11 19:03:49 +0100

    signals + services + modules
    with a complete restructuring; clearing the form, well-defining the form input and saving it as string

--- InvestingCalc/src/app/user-input/user-input.component.ts
+++ InvestingCalc/src/app/user-input/user-input.component.ts
@@ -1,30 +1,31 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { type CalculateInvestment } from './user-input.model';
-import { FormsModule } from '@angular/forms';
+import { Component, signal } from '@angular/core';
+
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
-  standalone: true,
-  imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInvestment = signal('0');
+  enteredExpectedReturn = signal('5');
+  enteredDuration = signal('10');
 
-  @Output() calculate = new EventEmitter<CalculateInvestment>();
+  constructor(private investmentService: InvestmentService) {}
 
-  enteredInitialInvestment = 0;
-  enteredAnnualInvestment = 0;
-  enteredExpectedReturn = 0;
-  enteredDuration = 0;
-
-
-  onSubmit(){
-    this.calculate.emit({
-      initialInvestment: this.enteredInitialInvestment,
-      annualInvestment: this. enteredAnnualInvestment,
-      expectedReturn: this.enteredExpectedReturn,
-      duration: this.enteredDuration,
+  onSubmit() {
+    this.investmentService.calculateInvestmentResults({
+      initialInvestment: +this.enteredInitialInvestment(),
+      duration: +this.enteredDuration(),
+      expectedReturn: +this.enteredExpectedReturn(),
+      annualInvestment: +this.enteredAnnualInvestment(),
     });
+
+    this.enteredInitialInvestment.set('0');
+    this.enteredAnnualInvestment.set('0');
+    this.enteredExpectedReturn.set('5');
+    this.enteredDuration.set('10');
   }
 }
--- InvestingCalc/src/app/user-input/user-input.model.ts
+++
@@ -1,6 +0,0 @@
-export interface CalculateInvestment {
-    initialInvestment: number;
-    annualInvestment: number;
-    expectedReturn: number;
-    duration: number;
-}
