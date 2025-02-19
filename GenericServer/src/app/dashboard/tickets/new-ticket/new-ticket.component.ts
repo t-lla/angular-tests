a5d46bbeb395fc59fa5a3a98c08f22a3bff3edbd
t-lla <tomas.llano.miro@gmail.com>
2025-02-20 00:33:04 +0100

    extracting and accessing template elements
    accessed through viewChild as to clear the form, extracted with #(template variables)

--- GenericServer/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ GenericServer/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ControlComponent } from "../../../shared/control/control.component";
+import { FormsModule } from '@angular/forms';
+import { ButtonComponent } from '../../../shared/button/button.component';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ControlComponent],
+  imports: [ControlComponent, ButtonComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;  //passing the component/element/template... properties to the 'form' property
 
+  onSubmit(title: string, text: string /*, form:HTMLFormElement*/) {
+    // const enteredTitle = titleElement.value;
+    // console.log("Entered title: "+enteredTitle);
+    console.log(title);
+    console.log(text);
+
+    this.form?.nativeElement.reset();
+  }
 }
